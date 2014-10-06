9807eadb5e63125f139db7d595e045cda3ae01e0
James <jamesghill@yahoo.co.uk>
2014-10-06 21:02:54 +0100

    First Version
    Just begun

---
+++ BBK - PiJ/src/Lesson_1_8_Maximiser.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Lesson_1_8_Maximiser {
+
+    public static void main(String[] args) {
+        
+        Scanner s = new Scanner(System.in);
+        String str;
+        int temp = 0;
+        int max = 0;
+        
+        System.out.println("Please enter a string of positve integers.");
+        System.out.println("Please enter '-1' to end and find the maximum number you entered.");
+        
+        while(true){
+            System.out.print("Please enter your first number: ");
+            str = s.nextLine();
+            temp = Integer.parseInt(str);
+            
+            if(temp == -1){
+                break;
+            }
+            
+            if(temp > max){
+                max = temp;
+            }
+            
+        }
+        
+        System.out.println("Your maximum number was " + max);
+        
+    }
+    
+}
