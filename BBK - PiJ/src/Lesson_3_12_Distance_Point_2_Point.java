0f8241132fe9528b310b12b0392ac2caec4576b6
James <jamesghill@yahoo.co.uk>
2014-10-21 17:47:02 +0100

    2nd Version
    Started work on the calculation of distances

--- BBK - PiJ/src/Lesson_3_12_Distance_Point_2_Point.java
+++ BBK - PiJ/src/Lesson_3_12_Distance_Point_2_Point.java
@@ -16,6 +16,8 @@ public class Lesson_3_12_Distance_Point_2_Point {
         Point point2 = new Point();
         Point point3 = new Point();
         
+        double dist1, dist2, dist3;
+        
         System.out.println("Please enter x coordinate number 1 : ");
         point1.x = s.nextDouble();
         System.out.println("Please enter y coordinate number 1 : ");
@@ -29,6 +31,18 @@ public class Lesson_3_12_Distance_Point_2_Point {
         System.out.println("Please enter y coordinate number 3 : ");
         point3.y = s.nextDouble();
         
+        dist1 = Math.sqrt((point1.x * point1.x) + (point1.y * point1.y));
+        dist2 = Math.sqrt((point2.x * point2.x) + (point2.y * point2.y));
+        dist3 = Math.sqrt((point3.x * point3.x) + (point3.y * point3.y));
+        
+        if(dist1 < dist2){
+            if(dist1 < dist3){
+                System.out.println("Point 1 is closest.");
+            }
+            else{
+                System.out.println("Point 3 is closest.");
+            }
+        }
         
         
     }
