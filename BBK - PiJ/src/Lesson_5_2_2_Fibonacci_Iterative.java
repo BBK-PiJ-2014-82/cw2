0976378cd301764736a270ef4235f63c3f7a66fa
James <jamesghill@yahoo.co.uk>
2014-11-10 20:23:41 +0000

    2nd Version
    Corrected the spelling of Fibonacci because I'm anal

--- BBK - PiJ/src/Lesson_5_2_2_Fibonacci_Iterative.java
+++ BBK - PiJ/src/Lesson_5_2_2_Fibonacci_Iterative.java
@@ -7,22 +7,22 @@ public class Lesson_5_2_2_Fibonacci_Iterative {
         Scanner s = new Scanner(System.in);
         System.out.print("Enter your integer here: ");
         int yourInt = s.nextInt();
-        int fibonnacci = 1;
+        int fibonacci = 1;
         int f1 = 1;
         int f2 = 1;
         
         if(yourInt <= 2){
-            fibonnacci = 1;
+            fibonacci = 1;
         }
         else{
             for(int i = 2; i < yourInt; i++){
-                fibonnacci = f1 + f2;
+                fibonacci = f1 + f2;
                 f1 = f2;
-                f2 = fibonnacci;
+                f2 = fibonacci;
             }
         }
         
-        System.out.println(fibonnacci);
+        System.out.println(fibonacci);
     }
 
 }
