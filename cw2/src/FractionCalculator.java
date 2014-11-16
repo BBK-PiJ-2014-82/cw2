e06fa4244b32057053516c5e98176834cd814fc2
James <jamesghill@yahoo.co.uk>
2014-11-16 21:28:50 +0000

    2nd Version
    Modified FractionCalculator  evaluator to parse basic string fractions.

--- cw2/src/FractionCalculator.java
+++ cw2/src/FractionCalculator.java
@@ -1,11 +1,41 @@
+import java.util.Scanner;
+
 public class FractionCalculator {
 
     public FractionCalculator(){}
     
-    public Fraction evaluate(Fraction fraction, String inputString){
-        char operator;
+    public Fraction evaluate(Fraction fraction, String inputString) {
         
-        return fraction;
+        // create variables
+        char operator;
+        Fraction current = fraction;
+        String item;
+        
+        // create Scanner
+        Scanner s1 = new Scanner(inputString);
+        
+        // parse the string and perform calculations
+        while(s1.hasNext()){
+            item = s1.next();
+            
+            // parse numerals
+            if(Character.isDigit(item.charAt(0))){
+                // create variables
+                int num, denom;
+                
+                // create Scanner
+                Scanner s2 = new Scanner(item);
+                s2.useDelimiter("/");
+                
+                num = s2.nextInt();
+                denom = s2.nextInt();
+                
+                current = new Fraction(num, denom);
+                
+            }
+        }
+        
+        return current;
     }
     
 }
