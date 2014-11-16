2a5d72c62514631555029637890494b4ad5e5ad7
James <jamesghill@yahoo.co.uk>
2014-11-16 19:46:53 +0000

    1st Version
    Created class for the FractionCalculator.

---
+++ cw2/src/FractionCalculator.java
@@ -0,0 +1,11 @@
+public class FractionCalculator {
+
+    public FractionCalculator(){}
+    
+    public Fraction evaluate(Fraction fraction, String inputString){
+        char operator;
+        
+        return fraction;
+    }
+    
+}
