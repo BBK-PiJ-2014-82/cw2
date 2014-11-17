7a8d2a47883f511e14f43002a75da2e355b790b4
James <jamesghill@yahoo.co.uk>
2014-11-17 17:03:55 +0000

    3rd Version
    Modified FractionCalculator test to check calculator parses string whole numbers.

--- cw2/src/FractionCalculatorTest.java
+++ cw2/src/FractionCalculatorTest.java
@@ -6,9 +6,27 @@ public class FractionCalculatorTest {
         
         // test fraction is entered and returned
         Fraction a = new Fraction(1,2);
-        Fraction b = calc.evaluate(a, "2,3");
-        System.out.println(b.toString());
+        Fraction b1 = calc.evaluate(a, "2/3");
+        Fraction b2 = calc.evaluate(a, "2/3");
+        test(b1, b2, "Fraction pass not successful: ", "Fraction pass successfull: ");
+        
+        // test whole number is converted into fraction with denominator 1
+        Fraction c = calc.evaluate(a, "2/1");
+        Fraction d = calc.evaluate(a, "2");
+        test(c, d, "Whole number conversion failed: ", "Whole number conversion successful: ");
+        
+        // test addition
+        //Fraction c = calc.evaluate(a, "+ 2/4");
+        //System.out.println("Fraction c is equal to " + c);
         
     }
-
+    
+    static void test(Fraction f1, Fraction f2, String msg1, String msg2){
+    	if (!f1.equals(f2)){
+            System.out.println(msg1 + f1.toString());
+        }
+        else{
+            System.out.println(msg2 + f1.toString());
+        }
+    }
 }
