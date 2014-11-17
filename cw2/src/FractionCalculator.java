d62381fc41bd210ebc7131a04e2ed8e334e6a21d
James <jamesghill@yahoo.co.uk>
2014-11-17 19:34:57 +0000

    4th Version
    Added a main method and launch method to FractionCalculator.

--- cw2/src/FractionCalculator.java
+++ cw2/src/FractionCalculator.java
@@ -1,8 +1,41 @@
 import java.util.Scanner;
 
 public class FractionCalculator {
-
+    
     public FractionCalculator(){}
+    
+    public static void main(String[] args) {
+        FractionCalculator calculator = new FractionCalculator();
+        calculator.launch();
+    }
+    
+    private void launch(){
+        
+        // variables
+        String input = "";
+        Fraction value = new Fraction(1,1);
+        
+        // create Scanner
+        Scanner scan = new Scanner(System.in);
+        
+        // welcome message
+        System.out.println("Welcome to James Hills' Fraction Calculator.");
+        System.out.println("Please enter a calculation below:");
+        
+        // obtain input
+        input = scan.nextLine();
+        
+        do  {
+            
+            evaluate(value, input);
+            System.out.println(" = " + value.toString());
+            input = scan.nextLine();
+            
+            } while (!input.equalsIgnoreCase("Goodbye"));
+        
+        System.out.println();
+        System.out.println("Fraction Calculator now closing.");
+    }
     
     public Fraction evaluate(Fraction fraction, String inputString) {
         
@@ -14,33 +47,45 @@ public class FractionCalculator {
         // create Scanner
         Scanner s1 = new Scanner(inputString);
         
+        //String str = System.console().readLine();
+        
         // parse the string and perform calculations
         while(s1.hasNext()){
             item = s1.next();
             
             // parse numerals
             if(Character.isDigit(item.charAt(0))){
-                // create variables
-                int num, denom;
-                
-                // create Scanner
-                Scanner s2 = new Scanner(item);
-                s2.useDelimiter("/");
-                
-                num = s2.nextInt();
-                
-                if(s2.hasNext()){
-                    denom = s2.nextInt();
-                    current = new Fraction(num, denom);
+                current = parseNumerals(item);
+            }
+            else{
+                switch(item){
+                    case "+": operator = item.charAt(0);
                 }
-                else{
-                    current = new Fraction(num, 1);
-                }
-                
             }
         }
         
         return current;
     }
     
+    private Fraction parseNumerals(String item){
+        // create variables
+        int num, denom;
+
+        // create Scanner
+        Scanner s2 = new Scanner(item);
+        s2.useDelimiter("/");
+        
+        // get 1st integer
+        num = s2.nextInt();
+        
+        // check for 2nd integer & return Fraction
+        if(s2.hasNext()){
+            denom = s2.nextInt();
+            return new Fraction(num, denom);
+        }
+        else{
+            return new Fraction(num, 1);
+        }
+    }
+
 }
